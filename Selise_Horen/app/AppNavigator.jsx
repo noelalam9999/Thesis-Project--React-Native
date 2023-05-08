80406ec4e63e352b2e88849baae27b555925e2fd
Sajon <sajon5271@gmail.com>
2023-05-08 11:04:21 +0600

    Merge conflicts fixed

--- Selise_Horen/app/AppNavigator.jsx
+++ Selise_Horen/app/AppNavigator.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import "react-native-gesture-handler";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { FontAwesome } from "@expo/vector-icons";
-import HomeScreen from "./screen/HomeScreen";
-import ProfileScreen from "./screen/profilescreen";
-import NotificationScreen from "./screen/NotificationScreen";
-import LoginScreen from "./screen/LoginScreen";
-import SignupScreen from "./screen/SignupScree";
-import LeaderboardScreen from "./screen/leaderboardScreen";
-import { MaterialIcons } from "@expo/vector-icons";
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItem,
-} from "@react-navigation/drawer";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { StatusBar, StyleSheet } from "react-native";
-=======
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -29,7 +9,6 @@ import SignupScreen from './screen/SignupScree';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { StatusBar } from 'react-native';
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
 const Tab = createMaterialBottomTabNavigator();
 
 const Drawer = createDrawerNavigator();
@@ -37,24 +16,10 @@ const Drawer = createDrawerNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-<<<<<<< HEAD
-      barStyle={{ backgroundColor: "black" }}
-      screenOptions={({ route }) => ({
-        title: "", // This is the main part...
-        tabBarIcon: ({ focused, color, size }) =>
-          GiveIcon({ route, focused, color, size }),
-      })}
-      tabBarOptions={{
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
-      }}
-    >
-=======
       barStyle={{ backgroundColor: 'black' }}
       tabBarOptions={{
         activeTintColor: 'white',
       }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
       <Tab.Screen
         name="Home2"
         component={HomeScreen}
@@ -73,11 +38,7 @@ const TabNavigator = () => {
             <MaterialIcons name="leaderboard" size={24} color="#F7CF47" />
           ),
           tabBarLabelStyle: {
-<<<<<<< HEAD
-            color: "white",
-=======
             fontWeight: 'bold',
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
           },
         }}
       />
@@ -87,30 +48,11 @@ const TabNavigator = () => {
 
 const AppNavigator = () => {
   return (
-<<<<<<< HEAD
-    <NavigationContainer>
-=======
     <NavigationContainer style={{ backgroundColor: 'black' }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
       <StatusBar barStyle="light-content" backgroundColor="black" />
       <Drawer.Navigator
         screenOptions={{
           drawerStyle: {
-<<<<<<< HEAD
-            backgroundColor: "#000000",
-          },
-          drawerActiveTintColor: "white",
-          drawerInactiveTintColor: "white",
-          headerStyle: {
-            backgroundColor: "#000000",
-          },
-          headerTintColor: "#F7CF47",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      >
-=======
             backgroundColor: 'black',
           },
           drawerActiveTintColor: 'white',
@@ -123,7 +65,6 @@ const AppNavigator = () => {
             fontWeight: 'bold',
           },
         }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
         <Drawer.Screen
           name="Home"
           options={{
