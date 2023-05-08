46332f0d008b7559b5c50c780f09d08ac92a57a0
san-shor <sanjidaewu89@gmail.com>
2023-05-08 11:27:54 +0600

    error fixed

--- Selise_Horen/app/AppNavigator.jsx
+++ Selise_Horen/app/AppNavigator.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -17,19 +16,7 @@ import {
 } from "@react-navigation/drawer";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { StatusBar, StyleSheet } from "react-native";
-=======
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import HomeScreen from './screen/HomeScreen';
-import ProfileScreen from './screen/profilescreen';
-import NotificationScreen from './screen/NotificationScreen';
-import LoginScreen from './screen/LoginScreen';
-import SignupScreen from './screen/SignupScree';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { StatusBar } from 'react-native';
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
+
 const Tab = createMaterialBottomTabNavigator();
 
 const Drawer = createDrawerNavigator();
@@ -37,31 +24,18 @@ const Drawer = createDrawerNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-<<<<<<< HEAD
       barStyle={{ backgroundColor: "black" }}
-      screenOptions={({ route }) => ({
-        title: "", // This is the main part...
-        tabBarIcon: ({ focused, color, size }) =>
-          GiveIcon({ route, focused, color, size }),
-      })}
       tabBarOptions={{
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
+        activeTintColor: "white",
       }}
     >
-=======
-      barStyle={{ backgroundColor: 'black' }}
-      tabBarOptions={{
-        activeTintColor: 'white',
-      }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
       <Tab.Screen
         name="Home2"
         component={HomeScreen}
         options={{
           tabBarIcon: () => <Icon name="home" size={24} color="#F7CF47" />,
           tabBarLabelStyle: {
-            color: 'white',
+            color: "white",
           },
         }}
       />
@@ -73,11 +47,7 @@ const TabNavigator = () => {
             <MaterialIcons name="leaderboard" size={24} color="#F7CF47" />
           ),
           tabBarLabelStyle: {
-<<<<<<< HEAD
-            color: "white",
-=======
-            fontWeight: 'bold',
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
+            fontWeight: "bold",
           },
         }}
       />
@@ -87,43 +57,24 @@ const TabNavigator = () => {
 
 const AppNavigator = () => {
   return (
-<<<<<<< HEAD
-    <NavigationContainer>
-=======
-    <NavigationContainer style={{ backgroundColor: 'black' }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
+    <NavigationContainer style={{ backgroundColor: "black" }}>
       <StatusBar barStyle="light-content" backgroundColor="black" />
       <Drawer.Navigator
         screenOptions={{
           drawerStyle: {
-<<<<<<< HEAD
-            backgroundColor: "#000000",
+            backgroundColor: "black",
           },
           drawerActiveTintColor: "white",
           drawerInactiveTintColor: "white",
           headerStyle: {
-            backgroundColor: "#000000",
+            backgroundColor: "black",
           },
-          headerTintColor: "#F7CF47",
+          headerTintColor: "white",
           headerTitleStyle: {
             fontWeight: "bold",
           },
         }}
       >
-=======
-            backgroundColor: 'black',
-          },
-          drawerActiveTintColor: 'white',
-          drawerInactiveTintColor: 'white',
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: 'white',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
->>>>>>> 52afe717ee4db9947f9b9eb57ac400adef566b52
         <Drawer.Screen
           name="Home"
           options={{
