8c9a849ed09d556ad95d41e0e72961793c222422
san-shor <sanjidaewu89@gmail.com>
2023-05-19 11:20:26 +0600

    sales url add

--- Selise_Horen/app/config.js
+++ Selise_Horen/app/config.js
@@ -1,9 +1,9 @@
 export default Config = {
   Backend: {
-    Device: '',
-    Users: '',
-    Signals: '',
+    Device: "",
+    Users: "",
+    Signals: "",
   },
-  API_URL: 'https://894e-59-152-97-85.ngrok-free.app',
-  USER_URL: 'https://3e14-59-152-97-85.ngrok-free.app',
+  API_URL: "https://8ea0-59-152-97-85.ngrok-free.app", //sales server
+  USER_URL: "https://3e14-59-152-97-85.ngrok-free.app",
 };
